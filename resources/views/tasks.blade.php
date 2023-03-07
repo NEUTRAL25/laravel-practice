53751bbc308ac4cd695b62be81d714cd4786781b
jfrancisco <jeffreyendrinafrancisco@gmail.com>
2023-03-07 22:45:47 +0800

    modified task controller, app.blaade.php, and task.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="{{ asset('css/styles.css') }}" >
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" crossorigin="anonymous">
+
 @extends('layouts.app')
  
 @section('content')
@@ -9,32 +12,34 @@
         @include('common.errors')
  
         <!-- New Task Form -->
-        <form action="/task" method="POST" class="form-horizontal">
+        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+            
             {{ csrf_field() }}
  
             <!-- Task Name -->
             <div class="form-group">
-                <label for="task" class="col-sm-3 control-label">Task</label>
+                <label for="task" class="col-sm-3 control-label">Task Name:</label>
  
                 <div class="col-sm-6">
                     <input type="text" name="name" id="task-name" class="form-control">
                 </div>
             </div>
+
+            <br>
  
             <!-- Add Task Button -->
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Task
+                        <i class="fa fa-plus"></i> Add a Task
                     </button>
                 </div>
             </div>
         </form>
     </div>
- 
-    <!-- TODO: Current Tasks -->
-     <!-- Current Tasks -->
-     @if (count($tasks) > 0)
+    
+    <!-- Current Tasks -->
+    @if (count($tasks) > 0)
         <div class="panel panel-default">
             <div class="panel-heading">
                 Current Tasks
@@ -46,25 +51,30 @@
                     <!-- Table Headings -->
                     <thead>
                         <th>Task</th>
+                        <th>Created on</th>
                         <th>&nbsp;</th>
                     </thead>
  
                     <!-- Table Body -->
-                    <tbody>
+                    <tbody class="tbody">
                         @foreach ($tasks as $task)
                             <tr>
                                 <!-- Task Name -->
                                 <td class="table-text">
                                     <div>{{ $task->name }}</div>
                                 </td>
- 
-                                <td>
-                                    <!-- TODO: Delete Button -->
-                                    <form action="/task/{{ $task->id }}" method="POST">
+                                <td class="table-text">
+                                    <div>{{ $task->created_at->format('m-d-Y H:i:s') }}</div>
+                                </td>
+                                <!-- Delete Button -->
+                                <td class="delete-button">
+                                    <form action="{{ url('task/'.$task->id) }}" method="POST" id="delete-task-{{ $task->id }}">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <button>Delete Task</button>
+
+                                        <button type="submit" class="btn btn-danger delete-task" data-task-id="{{ $task->id }}">
+                                        <i class="fa fa-trash"></i> Remove Task
+                                        </button>
                                     </form>
                                 </td>
                             </tr>
@@ -74,4 +84,7 @@
             </div>
         </div>
     @endif
+
+    <script src="{{ asset('js/delete-task.js') }}"></script>
+
 @endsection
