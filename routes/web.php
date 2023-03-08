dd8e2c162cdbb8d6e231b13c3e6f546397da6568
jfrancisco_3 <95389980+NEUTRAL25@users.noreply.github.com>
2023-03-08 18:23:49 +0800

    partial commit for adjustments

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,14 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/task', [TaskController::class, 'store']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::delete('/task/{task}', [TaskController::class, 'destroy']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
