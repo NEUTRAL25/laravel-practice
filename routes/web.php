22e2f60206c9bb006fcc07505219d7b80ebd96aa
jfrancisco_3 <95389980+NEUTRAL25@users.noreply.github.com>
2023-03-08 13:35:50 +0800

    another one

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', [TaskController::class, 'index']);
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/task', [TaskController::class, 'store']);
 Route::post('/tasks', [TaskController::class, 'store']);
-Route::delete('/task/{task}', [TaskController::class, 'destroy'];
+Route::delete('/task/{task}', [TaskController::class, 'destroy']);
