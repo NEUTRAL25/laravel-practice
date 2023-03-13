604e899171261d8e37e4e4855969cf1b6325d394
jfrancisco_3 <95389980+NEUTRAL25@users.noreply.github.com>
2023-03-13 17:59:32 +0800

    added a new feature for authentication and authorization

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', [TaskController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/task', [TaskController::class, 'store']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::delete('/task/{task}', [TaskController::class, 'destroy']);
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
